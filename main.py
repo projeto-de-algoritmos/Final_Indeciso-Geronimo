f7d300222aacd607db805075c8c137ec8c5716a4
luisgaboardi <luis.ggaboardi@gmail.com>
2020-12-11 00:27:35 -0300

    Adicionada imagem do grafo e interface gráfica inicial
    E interação com as funções previamente desenvolvidas

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from PIL import ImageTk, Image
 import os
 import sys
 from timeit import default_timer as timer
-
+import matplotlib.pyplot as plt 
 
 class Grafo(): 
   
@@ -37,7 +37,7 @@ class Grafo():
             print(j, ", ", end='')
             return
         self.printCaminho(parent , parent[j]) 
-        print(j, ", ", end='') 
+        print(j, ", ", end='')
           
  
     def printResultado(self, dist, parent, src):
@@ -49,7 +49,7 @@ class Grafo():
             print("")
   
 
-    def dijkstra(self, src): 
+    def dijkstra(self, src, destination): 
   
         linha  = len(self.grafo) 
         coluna = len(self.grafo) 
@@ -74,18 +74,18 @@ class Grafo():
                         parent[i] = u 
   
   
-        # print the constructed distance array 
-        self.printResultado(dist,parent,src)
+        #self.printResultado(dist,parent,src)
+        return str(dist[destination]), parent
 
 
-    def printArr(self, dist):
+    def printArr(self, dist, destination):
 
         print("Vértice \tDistância até Source")  
         for i in range(1, self.V):  
-            print("{0} \t\t\t  {1}".format(i, dist[i]))
+            print("{0} \t\t\t  {1}".format(i, dist[destination]))
 
 
-    def BellmanFord(self, src):  
+    def BellmanFord(self, src, destination):  
    
         dist = [float("Inf")] * self.V  
         dist[src] = 0
@@ -95,83 +95,131 @@ class Grafo():
             for u, v, w in self.grafoDP:  
                 if dist[u] != float("Inf") and dist[u] + w < dist[v]:  
                     dist[v] = dist[u] + w  
-   
-        self.printArr(dist) 
 
+        return str(dist[destination])
 
-if __name__ == '__main__':
     
 
-    g = Grafo(9)
-    g.addEdge(0, 1, 4)
-    g.addEdge(0, 7, 8)
+g = Grafo(9)
+g.addEdge(0, 1, 4)
+g.addEdge(0, 7, 8)
 
-    g.addEdge(1, 0, 4)
-    g.addEdge(1, 2, 8)
-    g.addEdge(1, 1, 11)
+g.addEdge(1, 0, 4)
+g.addEdge(1, 2, 8)
+g.addEdge(1, 7, 11)
 
-    g.addEdge(2, 1, 8)
-    g.addEdge(2, 3, 7)
-    g.addEdge(2, 5, 4)
-    g.addEdge(2, 8, 2)
+g.addEdge(2, 1, 8)
+g.addEdge(2, 3, 7)
+g.addEdge(2, 5, 4)
+g.addEdge(2, 8, 2)
 
-    g.addEdge(3, 2, 7)
-    g.addEdge(3, 4, 9)
-    g.addEdge(3, 5, 14)
+g.addEdge(3, 2, 7)
+g.addEdge(3, 4, 9)
+g.addEdge(3, 5, 14)
 
-    g.addEdge(4, 3, 9)
-    g.addEdge(4, 5, 10)
+g.addEdge(4, 3, 9)
+g.addEdge(4, 5, 10)
 
-    g.addEdge(5, 2, 4)
-    g.addEdge(5, 3, 14)
-    g.addEdge(5, 4, 10)
-    g.addEdge(5, 6, 2)
+g.addEdge(5, 2, 4)
+g.addEdge(5, 3, 14)
+g.addEdge(5, 4, 10)
+g.addEdge(5, 6, 2)
 
-    g.addEdge(6, 5, 2)
-    g.addEdge(6, 7, 1)
-    g.addEdge(6, 8, 6)
+g.addEdge(6, 5, 2)
+g.addEdge(6, 7, 1)
+g.addEdge(6, 8, 6)
 
-    g.addEdge(7, 0, 8)
-    g.addEdge(7, 1, 11)
-    g.addEdge(7, 6, 1)
-    g.addEdge(7, 8, 7)
+g.addEdge(7, 0, 8)
+g.addEdge(7, 1, 11)
+g.addEdge(7, 6, 1)
+g.addEdge(7, 8, 7)
 
-    g.addEdge(8, 2, 2)
-    g.addEdge(8, 6, 6)
-    g.addEdge(8, 7, 7)
+g.addEdge(8, 2, 2)
+g.addEdge(8, 6, 6)
+g.addEdge(8, 7, 7)
+
+           # 0  1  2  3  4  5  6  7  8
+g.grafo = [ [0, 4, 0, 0, 0, 0, 0, 8, 0],   # 0
+            [4, 0, 8, 0, 0, 0, 0, 11, 0],  # 1
+            [0, 8, 0, 7, 0, 4, 0, 0, 2],   # 2
+            [0, 0, 7, 0, 9, 14, 0, 0, 0],  # 3
+            [0, 0, 0, 9, 0, 10, 0, 0, 0],  # 4
+            [0, 0, 4, 14, 10, 0, 2, 0, 0], # 5
+            [0, 0, 0, 0, 0, 2, 0, 1, 6],   # 6
+            [8, 11, 0, 0, 0, 0, 1, 0, 7],  # 7
+            [0, 0, 2, 0, 0, 0, 6, 7, 0]    # 8
+          ]
 
 
-    g.grafo = [ [0, 4, 0, 0, 0, 0, 0, 8, 0], 
-                [4, 0, 8, 0, 0, 0, 0, 11, 0], 
-                [0, 8, 0, 7, 0, 4, 0, 0, 2], 
-                [0, 0, 7, 0, 9, 14, 0, 0, 0],
-                [0, 0, 0, 9, 0, 10, 0, 0, 0],
-                [0, 0, 4, 14, 10, 0, 2, 0, 0],
-                [0, 0, 0, 0, 0, 2, 0, 1, 6],
-                [8, 11, 0, 0, 0, 0, 1, 0, 7],
-                [0, 0, 2, 0, 0, 0, 6, 7, 0] 
-              ]
+root = Tk()
+root.geometry("600x860")
+root.title("Algoritmos Ambiciosos x Programação Dinâmica")
+
+subtitulo = Label(root, text="\nBairo São Bernardo\n")
+subtitulo.pack()
+
+imagem = Image.open("image.png").resize((530, 300), Image.ANTIALIAS)
+imagem = ImageTk.PhotoImage(imagem)
+imgLabel = Label(image=imagem)
+imgLabel.pack(padx=35)
 
 
-    start = timer()
-    g.dijkstra(0)
-    end = timer()
-    time_dijkstra = end - start
-    print("Dijkstra = %.7f" % time_dijkstra, " seg")
+def buscar():
+    origem = int(opc_origem.get())
+    destino = int(opc_destino.get())
+    
+    dijkstra, parent = g.dijkstra(origem, destino)
+    bellmanford = g.BellmanFord(origem, destino)
 
-    start = timer()
-    g.BellmanFord(0)
-    end = timer()
-    time_bellmanford = end - start
-    print("BellmanFord = %.7f" % time_bellmanford, " seg")
+    resultDijkstra.config(text=dijkstra)
+    resultBellmanFord.config(text=bellmanford)
 
-    time_diff = abs(time_dijkstra - time_bellmanford)
 
-    if(time_dijkstra < time_bellmanford):
-        print("BellmanFord foi melhor por %.7f" % time_diff, " seg")
-    else:
-        print("Dijkstra foi melhor por %.7f" % time_diff, " seg")
+label = Label(text="\n\nEscolha o ponto de início:\n")
+label.pack()
+options = range(0,9)
+opc_origem = StringVar()
+opc_origem.set(options[0])
 
-    exit()
+possiveis_nos = OptionMenu(root, opc_origem, *options)
+possiveis_nos.pack(padx=100)
 
-    root.mainloop()
+label = Label(text="Escolha o ponto de destino:\n")
+label.pack()
+options = range(0,9)
+opc_destino = StringVar()
+opc_destino.set(options[0])
+
+possiveis_nos = OptionMenu(root, opc_destino, *options)
+possiveis_nos.pack(padx=100)
+
+fazerBuscas = Button(root, text="Gerar caminho mais curto", command=buscar)
+fazerBuscas.pack(pady=20)
+
+label = Label(
+    text="_________________________________________________________________")
+label.pack()
+
+label = Label(
+    text="\nRESULTADOS")
+label.pack()
+
+DijkstraLabel = Label(root, text="\nDijkstra:", pady=5)
+DijkstraLabel.pack()
+global resultDijkstra
+resultDijkstra = Label(root, text="", wraplength=600, pady=5, padx=20)
+resultDijkstra.pack()
+
+BellmanFordLabel = Label(root, text="BellmanFord:", pady=5)
+BellmanFordLabel.pack()
+global resultBellmanFord
+resultBellmanFord = Label(root, text="", wraplength=600, pady=5, padx=20)
+resultBellmanFord.pack()
+
+CaminhoLabel = Label(root, text="Caminho percorrido:", pady=5)
+CaminhoLabel.pack()
+global Caminho
+Caminho = Label(root, text="", wraplength=600, pady=5, padx=20)
+Caminho.pack()
+
+root.mainloop()
